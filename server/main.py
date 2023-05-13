1e140ab6a9b42f9048190ec136ccba3bbb280de3
terra2007 <terra2007@naver.com>
2023-05-13 15:31:41 +0900

    ADD main

---
+++ server/main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+from server.app import api_router
+
+app = FastAPI()
+app.include_router(api_router)
+
+if __name__ == '__main__':
+    uvicorn.run("main:app", reload=True)
