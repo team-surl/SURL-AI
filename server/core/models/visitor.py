affebbd1a38cf5d14866a33dcb288e95e66f2047
terra2007 <terra2007@naver.com>
2023-05-16 20:20:10 +0900

    MODIFY visitor entity

--- server/core/models/visitor.py
+++ server/core/models/visitor.py
@@ -5,8 +5,8 @@ from server.core import Base
 
 class Visitor(Base):
     __tablename__ = 'tbl_visitor'
-    id = Column(UUID(as_uuid=True), primary_key=True)
-    url_id = Column(UUID(as_uuid=True), ForeignKey('tbl_url.id'), nullable=False)
+    id = Column(String(36), primary_key=True)
+    url_id = Column(String(36), ForeignKey('tbl_url.id'), nullable=False)
     ip = Column(String(50), nullable=False)
     country = Column(String(100), nullable=False)
     created_at = Column(TIMESTAMP, nullable=False)
