3f059c65325a82a07d4443675a3cfad5f603f800
terra2007 <terra2007@naver.com>
2023-05-16 20:20:01 +0900

    ADD statistice service

---
+++ server/utils/statistics.py
@@ -0,0 +1,72 @@
+from sqlalchemy.orm import Session
+from sqlalchemy import func
+from server.core.models.visitor import Visitor
+from server.core.models.url import URL
+from datetime import datetime, timedelta
+from collections import defaultdict
+
+
+def day(surl: str, session: Session):
+    today = datetime.now().date()
+    one_week_ago = today - timedelta(days=7)
+
+    visitors_by_date = {one_week_ago + timedelta(days=x): 0 for x in range(8)}
+
+    visits = (
+        session.query(func.date(Visitor.created_at), func.count(Visitor.id))
+        .join(URL)
+        .filter(URL.short_url == surl, Visitor.created_at >= one_week_ago)
+        .group_by(func.date(Visitor.created_at))
+        .all()
+    )
+
+    visitors_by_date.update(visits)
+
+    return visitors_by_date
+
+
+def week(surl: str, session: Session):
+    today = datetime.now().date()
+    seven_weeks_ago = today - timedelta(weeks=7)
+
+    visitors_by_week = {seven_weeks_ago + timedelta(weeks=x): 0 for x in range(8)}
+
+    visits = (
+        session.query(
+            func.DATE_FORMAT(Visitor.created_at, '%Y-%m-%d'),
+            func.count(Visitor.id)
+        )
+        .join(URL)
+        .filter(
+            URL.short_url == surl,
+            Visitor.created_at >= seven_weeks_ago
+        )
+        .group_by(
+            func.DATE_FORMAT(Visitor.created_at, '%Y-%m-%d')
+        )
+        .all()
+    )
+
+    for visit in visits:
+        week_start_date = datetime.strptime(visit[0], '%Y-%m-%d').date()
+        visitor_count = visit[1]
+        visitors_by_week[week_start_date] = visitor_count
+
+    return visitors_by_week
+
+
+def world(surl: str, session: Session):
+    uuid = session.query(URL.id).filter(URL.short_url == surl).first()[0]
+
+    current_time = datetime.now()
+    one_week_ago = current_time - timedelta(days=7)
+
+    stats = defaultdict(int)
+
+    visits = session.query(Visitor.country).filter(Visitor.url_id == uuid, Visitor.created_at >= one_week_ago).all()
+
+    for visit in visits:
+        country = visit[0].split(';')[3]
+        stats[country] += 1
+
+    return dict(stats)
