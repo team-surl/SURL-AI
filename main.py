6376517e7389b188e8e372b4266b83b4fce19a7c
terra2007 <terra2007@naver.com>
2023-05-17 13:51:17 +0900

    MODIFY CORS

--- main.py
+++ main.py
@@ -6,5 +6,13 @@ from server.app import api_router
 app = FastAPI()
 app.include_router(api_router)
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 if __name__ == '__main__':
-    uvicorn.run("main:app", reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", reload=True)
