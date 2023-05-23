6ac77fbfb279ee8e8bd0314640490b71f75605e0
terra2007 <terra2007@naver.com>
2023-05-23 14:47:46 +0900

    MODIFY prefix

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -5,4 +5,4 @@ from server.app.statistics import statistics_router
 api_router = APIRouter()
 
 api_router.include_router(code_router, tags=["code"])
-api_router.include_router(statistics_router, prefix="/statistics", tags=["statistics"])
+api_router.include_router(statistics_router, prefix="/stats", tags=["statistics"])
