b782833ae044050bbf46802b46e3fb37b1d8edee
terra2007 <terra2007@naver.com>
2023-05-23 14:48:42 +0900

    MODIFY main statistics

--- server/app/statistics.py
+++ server/app/statistics.py
@@ -1,7 +1,7 @@
 from server.core import session_scope
 
 from fastapi import APIRouter
-from server.utils.statistics import day, week, world
+from server.utils.statistics import day, week, world, main
 
 statistics_router = APIRouter()
 
@@ -24,4 +24,11 @@ def week_statistics(surl: str):
 def world_statistics(surl: str):
     with session_scope() as session:
         response = world(surl, session)
+    return response
+
+
+@statistics_router.get("/main")
+def main_statistics():
+    with session_scope() as session:
+        response = main(session)
     return response
